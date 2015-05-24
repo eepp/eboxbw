4a1f23a691ce0ec666257167bc7f3c4932b13216
Philippe Proulx <eeppeliteloop@gmail.com>
2015-05-24 12:32:11 -0400

    eboxbw v2.2

--- eboxbw/__init__.py
+++ eboxbw/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-__version__ = '2.0'
+__version__ = '2.2'
 
 
 from .eboxbw import Error, DownloadError, InvalidPageError, \
