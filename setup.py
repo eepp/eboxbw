cc7016841317a78a9ae0d9456264b22b01a9b8d3
Philippe Proulx <eeppeliteloop@gmail.com>
2017-05-29 11:34:41 -0400

    eboxbw v2.3.4
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ entry_points = {
 
 
 setup(name='eboxbw',
-      version='2.3.3',
+      version='2.3.4',
       description='Electronic Box cable Internet bandwidth usage',
       author='Philippe Proulx',
       author_email='eeppeliteloop@gmail.com',
